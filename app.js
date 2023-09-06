c6f53bc55db12579b3617894adc2238d88203278
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-06 20:04:31 -0300

    Concentrando rotas

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
-//Importando o express
 const express = require("express");
-const app = express();
+const checkListRouter = require("./src/routes/checklist");
 
+const app = express();
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-};
+app.use("/checklists", checkListRouter);
 
-app.use(log);
-
-//Setando as rotas
-app.get("/", (req, res) => {
-  res.send("<h1>Minha lista de tarefas</h1>");
-});
-
-app.get("/json", (req, res) => {
-  res.json({
-    tasks: [
-      { title: "Tarefa 1", done: true },
-      { title: "Tarefa 2", done: false },
-      { title: "Tarefa 3", done: false },
-    ],
-  });
-});
-
-//Dando start para ouvir na porta 3000
 app.listen(3000, () => {
   console.log("Servidor foi inicializado!");
 });
