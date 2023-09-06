f8f894e4acf1c807e99c60cb9a7fdf357848e28d
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-06 18:57:09 -0300

    Respostas JSON

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
   res.send("<h1>Minha lista de tarefas</h1>");
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    tasks: [
+      { title: "Tarefa 1", done: true },
+      { title: "Tarefa 2", done: false },
+      { title: "Tarefa 3", done: false },
+    ],
+  });
+});
+
 //Dando start para ouvir na porta 3000
 app.listen(3000, () => {
   console.log("Servidor foi inicializado!");
